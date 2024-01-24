e923502afd02c659fae40adc838303688df8bf07
Crowdbotics <engineering@crowdbotics.com>
2024-01-24 20:19:52 +0000

    Initial commit

---
+++ backend/gentle_snowflake_26/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for gentle_snowflake_26 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "gentle_snowflake_26.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gentle-snowflake-26",
+  url: "https://gentle-snowflake-26.botics.co",
+  api: "https://gentle-snowflake-26.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
